c4a955c193c8db3e94786b55c291e20ba0e6870e
Parthxdev <parthc0333@gmail.com>
2025-04-28 17:44:13 +0530

    Small Changes

--- calculator.py
+++ calculator.py
@@ -1,18 +1,19 @@
-# simple-calculator 
-a = int(input("type 1st value : "))
-c = input("What operation do you want (+, -, *, %, //): ")
-b = int(input("type 2nd value and enter : "))
+# Simple Calculator
 
-# here apply logic
-if c == "+":
-    print(f"result : {a} + {b} = {a+b}")
-elif c == "-":
-    print(f"result : {a} - {b} = {a-b}")
-elif c == "*":
-    print(f"result : {a} * {b} = {a*b}")
-elif c == "%":
-    print(f"result : {a} % {b} = {a%b}")
-elif c == "//":
-    print(f"result : {a} // {b} = {a//b}")
+a = int(input("Enter the first number: "))
+op = input("Choose an operation (+, -, *, %, //): ")
+b = int(input("Enter the second number: "))
+
+# Here Apply Logic
+if op == "+":
+    print(f"Result: {a} + {b} = {a + b}")
+elif op == "-":
+    print(f"Result: {a} - {b} = {a - b}")
+elif op == "*":
+    print(f"Result: {a} * {b} = {a * b}")
+elif op == "%":
+    print(f"Result: {a} % {b} = {a % b}")
+elif op == "//":
+    print(f"Result: {a} // {b} = {a // b}")
 else:
-    print(f"Invalid operator plz enter one of +, -, *, %, //")  
+    print("Invalid operator! Please use one of (+, -, *, %, //).")
